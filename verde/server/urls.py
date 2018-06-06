ae724cced5afba858218f841c76dc810b09a114c
teenoh <valentino@grit.systems>
2018-06-06 23:33:35 +0100

    display error on auth pages & add more routes

--- verde/server/urls.py
+++ verde/server/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name="logout"),
     url(r'^dashboard/$', dashboard, name="dashboard"),
     url(r'^register-farmer/$', register_farmer, name="register-farmer"),
+    url(r'^soil-test/$', register_farmer, name="soil-test"),
+    url(r'^sms/$', register_farmer, name="sms"),
+    url(r'^voice/$', register_farmer, name="voice"),
+    url(r'^reports/$', register_farmer, name="reports"),
     url(r'', index, name="landing"),
 ]
