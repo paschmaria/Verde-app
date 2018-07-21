5c50ee3a2d8b470f65e2181e8ef1d3dc902494b6
teenoh <valentino@grit.systems>
2018-07-21 15:20:47 +0100

    set urls for local dev

--- verde/urls.py
+++ verde/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,7 +26,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-        urlpatterns += static(settings.STATIC_URL,
-                              document_root=settings.STATIC_ROOT)
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
